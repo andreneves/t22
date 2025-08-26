c5b37b7d63407abc9733f3c0f436d50d7aefe160
andreneves <andr@andr.com.br>
2025-08-25 21:16:11 -0300

    aula4

---
+++ aula04.php
@@ -0,0 +1,14 @@
+<?php
+// Aula 04 - Operadores Relacionais e Lógicos
+//C:\xampp\htdocs\t22\aula04.php
+//http://localhost/t22/aula04.php
+
+$idade = 18;
+$estado = "RJ";
+
+if($idade == 18) {
+    echo("A idade é igual a 18 anos" . "<br>");
+}
+
+
+?>
