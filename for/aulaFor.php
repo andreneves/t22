cfac3bcbb984e6949cd09c9c1c43cc0df2e49347
andreneves <andr@andr.com.br>
2025-11-14 20:52:07 -0300

    teset

---
+++ for/aulaFor.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h3>Laço de Repetição For - incremento</h3><br>";
+
+for ($i = 1; $i <= 5; $i++) { 
+    echo("Número: $i <br>");
+}
+
+echo "<h3>Laço de Repetição For - decremento</h3><br>";
+
+for ($i = 10; $i >= 1; $i--) { 
+    echo("Número: $i <br>");
+}
+
+?>
